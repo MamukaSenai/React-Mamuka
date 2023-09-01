19847cb353a0907f821d85c859711d017fb86eae
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2023-08-31 21:55:34 -0300

    Testando interação

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Header from './components/Gestor/Header'
+import gestorCriarDemandas from './pages/Gestor/Criar Demandas'
+import gestorCriarProjetos from './pages/Gestor/Criar Projetos'
 
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
- 
+    <Routes>
+      <Route path='gestorCriarDemandas' element={<gestorCriarDemandas/>}/>
+      <Route path='gestorCriarProjetos' element={< gestorCriarProjetos />} />
+    </Routes>
   </React.StrictMode>,
 )
