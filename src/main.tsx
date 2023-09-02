ae8bbc73459fc9e7ac618d0bb36c837f2db0e7ec
Admir-Designer-Programador <134459063+Admir-Designer-Programador@users.noreply.github.com>
2023-09-01 22:01:15 -0300

    Correcao erros pagina

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
+// import App from './App'
+import { BrowserRouter, Routes } from 'react-router-dom'
+import Perfil from '../src/pages/Desenvolvedor/Perfil/index'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
- 
+        <BrowserRouter>
+
   </React.StrictMode>,
+
+      <Routes>
+        <Routes path='perfil/:idUsuario' element={ <Perfil /> } />
+      
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
