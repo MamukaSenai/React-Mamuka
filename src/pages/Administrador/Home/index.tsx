8cc1e718a62ac6471ca60ab2e7fca319ae2807ac
Amanda Aita <aamandinha.aita88@gmail.com>
2023-09-04 21:50:12 -0300

    continuacao

--- src/pages/Administrador/Home/index.tsx
+++ src/pages/Administrador/Home/index.tsx
@@ -1,7 +1,8 @@
+import Cadastro from "..//..//../assets/cadastro.png"
+import Consulta from "..//..//../assets/consulta.png"
 
 
-import Cadastro from "../../../assets/cadastro.png"
-import Consulta from "../../../assets/consulta.png"
+// import Consulta from "../../../assets/consulta.png"
 
 
 //rotas
@@ -16,8 +17,6 @@ import "style.css";
 function Home() {
 
   return (
-
-
 <>
   <meta charSet="UTF-8" />
   <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -26,8 +25,7 @@ function Home() {
   <link rel="stylesheet" href="administrador_home.css" />
   <aside>
     <nav className="menu">
-      <i>
-        <svg
+      <i><svg
           xmlns="http://www.w3.org/2000/svg"
           height="1em"
           viewBox="0 0 448 512"
@@ -48,18 +46,15 @@ function Home() {
       <section className="right">
         <div className="conteudo">
           <div className="cadastro">
-            <img src="../img/Cadastro.png" alt="" />
-            <a href="../cadastro1/cadastro1.html" className="button">
+            <img src={Cadastro} alt="cadastro" />
+            <a href="..//..//../assets/cadastro.png" className="button">
               {" "}
               Cadastre um no usuários
             </a>
           </div>
           <div className="consulta">
-            <img src="../img/Consulta.png" alt="" />
-            <a
-              href="../administrador_consulta/administrador_consulta.html"
-              className="button"
-            >
+            <img src={Consulta} alt="consulta" />
+            <a href="..//..//../assets/consulta.png" className="button">
               {" "}
               Consulte usuários cadastrados
             </a>
@@ -67,48 +62,13 @@ function Home() {
         </div>
       </section>
     </main>
-    <header className="topo">
-      <div className="logo">
-        <img src="../img/Logo.png" alt="logo" />
-      </div>
-      <div className="pesquisa">
-        {/*<i class="fa-solid fa-magnifying-glass" id="lupa"></i>*/}
-        <i>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 512 512"
-          >
-            {/*! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
-            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
-          </svg>
-        </i>
-        <input className="campotexto" type="text" placeholder="Pesquisa" />
-      </div>
-      <div className="ricardo">
-        <img src="../img/Ricardo.png" alt="" />
-        {/*<p>Ricardo Souza</p>*/}
-        <a href="../perfil_administrador/perfil_administrador.html">
-          Ricardo Souza
-        </a>
-        <i>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1em"
-            viewBox="0 0 320 512"
-          >
-            {/*! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. */}
-            <path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z" />
-          </svg>
-        </i>
-      </div>
-    </header>
+
   </div>
 </>
 
-  }
 
-  )
+)
 
+}
 
-export default Home;
+export default Home();
