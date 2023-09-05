e1c0bd17aa90075b26a393096c8926c6ad9db6de
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2023-09-04 21:54:29 -0300

    atualizacao

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+
+import gestorCriarProjetos from './pages/Gestor/Criar Projetos'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
- 
+    <BrowserRouter>
+
+      <Routes>
+        <Route/>
+          <Route path='gestorCriarProjetos' element={ < gestorCriarProjetos/> } />
+      </Routes>
+
+    </BrowserRouter>
   </React.StrictMode>,
 )
