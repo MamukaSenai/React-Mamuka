8305545604da7c9910b5d94164f70d285dab365b
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2023-09-06 16:02:09 -0300

    Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+
+
 
 import './index.css'
+import CadastroInfosPessoais from './pages/Administrador/CadastroInfosPessoais'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
- 
+    <BrowserRouter>
+
+      <Routes>
+        <Route/>
+          <Route path='cadastroInfosPessoais' element={ <CadastroInfosPessoais/> } />
+      </Routes>
+
+    </BrowserRouter>
   </React.StrictMode>,
 )
