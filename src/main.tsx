080279274ff8dc8fa85d0c17b68da3b6277f126a
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2023-09-06 21:17:06 -0300

    conclusao estatico

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+
+
 
 import './index.css'
+import CadastroInfosConclusao from './pages/Administrador/CadastroInfosConclusão'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
- 
+    <BrowserRouter>
+
+      <Routes>
+        <Route/>
+          <Route path='cadastroInfosConclusao' element={ <CadastroInfosConclusao/> } />
+      </Routes>
+
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- src/pages/Administrador/CadastroInfosConclusão/index.tsx
+++ src/pages/Administrador/CadastroInfosConclusão/index.tsx
@@ -1,48 +1,48 @@
 import "./style.css"
 
-function CadastroInfosConclusao(){
+function CadastroInfosConclusao() {
 
-    return(
+    return (
         <main>
             {/* <section class="right"> */}
             <div className="linha_sup_status">
                 <div className="linha_sup_esq">
-                <p>administrador / cadastro</p>
+                    <p>administrador / cadastro</p>
                 </div>
                 <div className="linha_sup_dir">
-                <a href="../cadastro1/cadastro1.html" className="destaque2">
-                    pessoal
-                </a>
-                {/* <p class="destaque">corporativo</p> */}
-                <a href="../cadastro2/cadastro2.html" className="destaque">
-                    corporativo
-                </a>
-                <a href="../cadastro3/cadastro3.html" className="destaque2">
-                    conclusão
-                </a>
+                    <a href="../cadastro1/cadastro1.html" className="destaque2">
+                        pessoal
+                    </a>
+                    {/* <p class="destaque">corporativo</p> */}
+                    <a href="../cadastro2/cadastro2.html" className="destaque">
+                        corporativo
+                    </a>
+                    <a href="../cadastro3/cadastro3.html" className="destaque2">
+                        conclusão
+                    </a>
                 </div>
             </div>
             <form className="formulario">
                 <div>
-                <img src="../img/PessoaCadastro.png" alt="" />
+                    <img src="../img/PessoaCadastro.png" alt="" />
                 </div>
                 <div className="label_mail">
-                <label htmlFor="campo-usuario">Perfil:</label>
-                <input placeholder="Administrador" name="usuario" id="campo-usuario" />
+                    <label htmlFor="campo-usuario">Perfil:</label>
+                    <input placeholder="Administrador" name="usuario" id="campo-usuario" />
                 </div>
                 <div className="label_mail">
-                <label htmlFor="campo-senha">Cargo:</label>
-                <input placeholder="Gerente" name="usuario" id="campo-usuario" />
+                    <label htmlFor="campo-senha">Cargo:</label>
+                    <input placeholder="Gerente" name="usuario" id="campo-usuario" />
                 </div>
                 <div className="label_mail">
-                <label htmlFor="campo-senha">Numero Usuario:</label>
-                <input placeholder="987874652023" name="usuario" id="campo-usuario" />
+                    <label htmlFor="campo-senha">Numero Usuario:</label>
+                    <input placeholder="987874652023" name="usuario" id="campo-usuario" />
                 </div>
                 <div>
-                <button type="submit">
-                    {" "}
-                    <a href="../cadastro3/cadastro3.html">salvar</a>{" "}
-                </button>
+                    <button type="submit">
+                        {" "}
+                        <a href="../cadastro3/cadastro3.html">salvar</a>{" "}
+                    </button>
                 </div>
             </form>
             {/* </section> */}
