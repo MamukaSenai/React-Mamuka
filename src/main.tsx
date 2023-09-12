e2f3a9db364262c1ff7bc93cf055a23344e4f248
Vinicius <viniciuszaneti.vz@gmail.com>
2023-09-11 21:54:21 -0300

    colocando o gráfico de pizza com o chartjs, falta estilizar e tentar colocar mais dados

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 
 import './index.css'
+import Dashboard from './pages/Gestor/Dashboard'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
     <Header />
+    <Routes>
+      <Route path="dashboard" element = {<Dashboard />}/>
+    </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ src/pages/Gestor/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import "./style.css"
+// import PieChart from "../../../components/GraficoDashboard";
+import Pies from "../../../components/GraficoDashboard";
+
+export default function Dashboard(){
+    return (
+        <div>
+          <Pies />
+        </div>
+      );
+ }
