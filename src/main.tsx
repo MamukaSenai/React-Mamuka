069bee151d31e29bf639f24c4e76e34fd3b2d5dc
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2023-09-14 14:38:37 -0300

    Ajuste na Route

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
 
-        <Route path='/' element={<Home />} />
+        <Route path='home' element={<Home />} />
       </Routes>
     </BrowserRouter>
 
