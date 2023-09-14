fb4af87b3fa0d9845735465c4f6d713eabf940d4
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2023-09-14 15:10:24 -0300

    conclusão Login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import './index.css'
+import GestorLogin from './pages/Gestor/Login'
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
- 
-  </React.StrictMode>,
+      <React.StrictMode>
+            <BrowserRouter>
+
+
+                  <Routes>
+                        <Route path='gestorLogin' element={<GestorLogin />} />
+
+                  </Routes>
+
+
+            </BrowserRouter>
+      </React.StrictMode>
 )
