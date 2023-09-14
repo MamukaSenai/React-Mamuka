c8284186fbebb59a9f98b5e6d781c1bbf76b79fe
Vinicius <viniciuszaneti.vz@gmail.com>
2023-09-13 21:55:01 -0300

    criando a tabela com a biblioteca do Material UI, precisamos costumizar e adicionar os dados corretos

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import './index.css'
+import EnhancedTable from './pages/Desenvolvedor/Demandas'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
- 
+    <BrowserRouter>
+      <Routes>
+        <Route path="tabela" element = {<EnhancedTable />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
