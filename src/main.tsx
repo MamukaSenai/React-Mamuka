1788f99d4daf1eb7cbd720d51b9bc8ecf131512c
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2023-09-14 15:01:28 -0300

    conclusão Login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import './index.css'
+import DesenvolvedorLogin from './pages/Desenvolvedor/Login'
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
- 
-  </React.StrictMode>,
+      <React.StrictMode>
+            <BrowserRouter>
+
+
+                  <Routes>
+                        <Route path='desenvolvedorLogin' element={<DesenvolvedorLogin/>} />
+
+                  </Routes>
+
+
+            </BrowserRouter>
+      </React.StrictMode>
 )
