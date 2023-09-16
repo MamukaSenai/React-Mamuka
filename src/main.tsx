66f6b9317fad7bfa566cc027e5ae0b903c2a0309
Vinicius <viniciuszaneti.vz@gmail.com>
2023-09-15 21:14:49 -0300

    resolvendo conflitos aside

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import Aside from './components/Aside'
 import Header from './components/Header'
-
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Header />
+      <Header />
+    <Aside />
     </BrowserRouter>
   </React.StrictMode>,
 )
