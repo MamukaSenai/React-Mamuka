3f3892bb8564babc013ac53da35509d646ca06c0
Admir-Designer-Programador <134459063+Admir-Designer-Programador@users.noreply.github.com>
2023-09-18 21:40:49 -0300

    Administrador_Perfil Final

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
+// import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Administradorperfil from '../src/pages/Administrador/Perfil/index'
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
- 
-  </React.StrictMode>,
+    <BrowserRouter>
+
+
+      <Routes>
+        <Route path='administradorperfil' element={<Administradorperfil />} />
+
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
