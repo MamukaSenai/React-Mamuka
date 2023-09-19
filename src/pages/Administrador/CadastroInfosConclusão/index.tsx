e30add5444333b5a84e016552bd70dae371053d0
jeffersonsouzasilva <92387696+jeffersonsouzasilva@users.noreply.github.com>
2023-09-19 17:13:25 -0300

    ajuste no css

--- src/pages/Administrador/CadastroInfosConclusão/index.tsx
+++ src/pages/Administrador/CadastroInfosConclusão/index.tsx
@@ -6,48 +6,53 @@ function CadastroInfosConclusao() {
     return (
         <main id="rightConclusao">
             <section id="CadastroConclusao">
-            {/* <section class="right"> */}
-            <div className="linha_sup_status">
-                <div className="linha_sup_esq">
-                    <p>administrador / cadastro</p>
+                <div className="linha_sup_status">
+                    <div className="linha_sup_esq">
+                        <p>administrador / cadastro</p>
+                    </div>
+                    <div className="linha_sup_dir">
+                        {/* <p>pessoal</p>  */}
+                        <a href="../cadastro1/cadastro1.html" className="destaque2">
+                            pessoal
+                        </a>
+                        {/* <p>corporativo</p> */}
+                        <a href="../cadastro2/cadastro2.html" className="destaque2">
+                            corporativo
+                        </a>
+                        {/* <p class="destaque">conclusao</p> */}
+                        <a href="../cadastro3/cadastro3.html" className="destaque">
+                            conclusão
+                        </a>
+                    </div>
                 </div>
-                <div className="linha_sup_dir">
-                    <a href="../cadastro1/cadastro1.html" className="destaque2">
-                        pessoal
-                    </a>
-                    {/* <p class="destaque">corporativo</p> */}
-                    <a href="../cadastro2/cadastro2.html" className="destaque">
-                        corporativo
-                    </a>
-                    <a href="../cadastro3/cadastro3.html" className="destaque2">
-                        conclusão
-                    </a>
-                </div>
-            </div>
-            <form className="formulario">
-                <div>
-                    <img src={PessoaCadastro} alt="" />
-                </div>
-                <div className="label_mail">
-                    <label htmlFor="campo-usuario">Perfil:</label>
-                    <input placeholder="Administrador" name="usuario" id="campo-usuario" />
-                </div>
-                <div className="label_mail">
-                    <label htmlFor="campo-senha">Cargo:</label>
-                    <input placeholder="Gerente" name="usuario" id="campo-usuario" />
-                </div>
-                <div className="label_mail">
-                    <label htmlFor="campo-senha">Numero Usuario:</label>
-                    <input placeholder="987874652023" name="usuario" id="campo-usuario" />
-                </div>
-                <div>
-                    <button type="submit">
-                        {" "}
-                        <a href="../cadastro3/cadastro3.html">salvar</a>{" "}
-                    </button>
-                </div>
-            </form>
-            {/* </section> */}
+                <form className="formulario">
+                    <div>
+                        <img src={PessoaCadastro} alt="" />
+                    </div>
+                    <div className="label_mail">
+                        <label htmlFor="campo-usuario">E-mail:</label>
+                        <input
+                            placeholder="ricardosouza1980@gmail.com"
+                            name="usuario"
+                            id="campo-usuario"
+                        />
+                    </div>
+                    <div className="label_mail">
+                        <label htmlFor="campo-senha">Confirme o E-mail:</label>
+                        <input
+                            placeholder="ricardosouza1980@gmail.com"
+                            name="usuario"
+                            id="campo-usuario"
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">
+                            {" "}
+                            <a href="../cadastro3/cadastro3.html">salvar</a>{" "}
+                        </button>
+                    </div>
+                </form>
+                {/* </section> */}
             </section>
         </main>
         
