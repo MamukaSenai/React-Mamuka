106e964d7fb589ff68d9f1ab6672075acc89f50f
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2023-09-20 21:52:43 -0300

    correcao no css

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ import CadastroInfosCorporativas from './pages/Administrador/CadastroInfosCorpor
 import CadastroInfosConclusao from './pages/Administrador/CadastroInfosConclusão'
 import DesenvolvedorPerfil from '../src/pages/Desenvolvedor/Perfil/index'
 import DesenvolvedorLogin from './pages/Desenvolvedor/Login'
-import DesenvolvedorDemandas from './pages/Desenvolvedor/Demandas'
+// import DesenvolvedorDemandas from './pages/Desenvolvedor/Demandas'
 import Dashboard from './pages/Gestor/Dashboard'
 
 
@@ -38,7 +38,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="admconsulta" element = {<AdministradorConsulta />}/>
         {/* <Route path="admperfil" element = {<AdministradorConsulta />}/> */}
         {/* PAGINAS DE DEV */}
-        <Route path="devdemandas" element = {< DesenvolvedorDemandas />}/>
+        {/* <Route path="devdemandas" element = {< DesenvolvedorDemandas />}/> */}
         <Route path="devlogin" element = {<DesenvolvedorLogin />}/>
         <Route path="devperfil" element = {<DesenvolvedorPerfil />}/> 
         {/* PAGINAS DE GESTOR */} 
