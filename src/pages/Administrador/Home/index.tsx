f990dbe0d59a05de112876c78c54e8a1f74bf186
Anderson Bertolo <21359810870@info.local>
2023-11-29 21:54:58 -0300

    Responsividade do menu

--- src/pages/Administrador/Home/index.tsx
+++ src/pages/Administrador/Home/index.tsx
@@ -19,39 +19,35 @@ function Home() {
   return (
     <>
 
-      <main id="right_home">
-        <section id="Home">
-          <section className="right">
-            <div className="conteudo">
-              <div className="cadastro">
-                <img src={Cadastro} alt="cadastro" />
-                <Link to="admpessoais" className="button">
-                  {/* <a href="#" className="button">
-                    {" "}
-                    Cadastre os usuários
-                  </a> */}
-                  Cadastre os usuários
-                </Link>
-                
-              </div>
-              <div className="consulta">
-                <img src={Consulta} alt="consulta" />
-                <Link to="admconsulta" className="button">
-                  {/* <a href="" className="button">
-                    {" "}
-                    Consulte usuários cadastrados
-                  </a> */}
-                  Consulte usuários cadastrados
-                </Link>
-                
-              </div>
-            </div>
-          </section>
-        </section>
+<main>
+        <div className="conteudo">
+                <div className="cadastro">
+                    <img src={Cadastro} alt="cadastro" />
+                        <Link to="admpessoais" className="button">
+                          {/* <a href="#" className="button">
+                            {" "}
+                            Cadastre os usuários
+                          </a> */}
+                          Cadastre os usuários
+                        </Link>
+                </div>
+
+
+                <div className="consulta">
+                    <img src={Consulta} alt="consulta" />
+                        <Link to="admconsulta" className="button">
+                          {/* <a href="" className="button">
+                            {" "}
+                            Consulte usuários cadastrados
+                          </a> */}
+                          Consulte usuários cadastrados
+                        </Link>
+                </div>
+
+        </div>
 
       </main>
     </>
-
 
   )
 
